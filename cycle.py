ec2839ca7e356398f704b72e20d9076eaf52d62e
_Sain <sander.lantin@outlook.com>
2017-09-06 22:56:50 +0200

    Update cycle.py

--- cycle.py
+++ cycle.py
@@ -1,4 +1,4 @@
-class custom_cycle:
+class cycle:
 	def __init__(self, user_string):
 		self.u_str = user_string
 		self.u_str_len = len(self.u_str)
@@ -7,8 +7,36 @@ class custom_cycle:
 		self.counter = 0
 
 
+	def __next__(self):
+		"""
+		Replaces the character in the buffer (u_str_buff) by the next character
+		Increases the counter by 1 
+		OR resets it to 0 if the counter is at the maximum number
+		else it would throw: IndexError: string index out of range
+		
+		Example usage:
+		cycle(['foo', 'bar'])
+		print(next(your_variable))
+
+		output:
+		foo
+		bar
+		foo
+		"""
+		self.u_str_buff = self.u_str[self.counter]
+
+		if self.counter < self.u_str_len:
+			self.counter += 1
+
+		if self.counter >= self.u_str_len:
+			self.counter = 0
+
+		return self.u_str_buff
+
+
 	def next_item(self):
 		"""
+		== Updated version for this is __next__() ==
 		Replaces the character in the buffer (u_str_buff) by the next character
 		Increases the counter by 1 
 		OR resets it to 0 if the counter is at the maximum number
@@ -30,59 +58,3 @@ class custom_cycle:
 		Returns the integer counter
 		"""
 		return self.counter
-
-
-
-##############################
-# TESTING USAGE AND EXAMPLES #
-##############################
-
-def main():
-	# Testing
-	x = custom_cycle('foobar')
-
-	for xx in range(15):
-		print(x.next_item())
-
-
-	test_array = ['hello', 'testing', 'foo', 'bar']
-	y = custom_cycle(test_array)
-
-	for yy in range(10):
-		print(y.next_item())
-		
-	"""
-	
-	Output:
-	
-	f
-	o
-	o
-	b
-	a
-	r
-	f
-	o
-	o
-	b
-	a
-	r
-	f
-	o
-	o
-	hello
-	testing
-	foo
-	bar
-	hello
-	testing
-	foo
-	bar
-	hello
-	testing
-	
-	"""
-
-
-if __name__ == '__main__':
-	main()
